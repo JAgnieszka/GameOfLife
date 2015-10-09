e1be0a426754e32dd7e1ccf353a5d443492dc178
somename <someemail>
2015-10-09 13:16:45 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestGrid.java
@@ -31,6 +31,24 @@ public class TestGrid {
 		grid.addCell(cell2);
 
 		int numNeighbors = grid.getNeighbors(cell1);
+
+		
+		assertEquals(1, numNeighbors);
+		
+	}
+	
+	@Test
+	public void testTwoNeighborsCellsOnDifferentRowsShouldHaveOneNeighbor() {
+		//arrange
+		Grid grid =new Grid();
+		Cell cell1= new Cell(0,0);
+		Cell cell2= new Cell(1,0);
+		//
+		grid.addCell(cell1);
+		grid.addCell(cell2);
+
+		int numNeighbors = grid.getNeighbors(cell1);
+
 		
 		assertEquals(1, numNeighbors);
 		
